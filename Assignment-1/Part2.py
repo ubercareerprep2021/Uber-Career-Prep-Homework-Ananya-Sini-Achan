ea5979b4f4a2e230483e8e98266de5e9c6f0a39b
Ananya Sini Achan <68202735+ananyaachan@users.noreply.github.com>
2021-05-06 20:48:20 +0400

    Update Part2.py

--- Assignment-1/Part2.py
+++ Assignment-1/Part2.py
@@ -5,20 +5,36 @@ Created on Thu Apr  1 18:26:12 2021
 
 @author: Ananya
 """
-
 def isStringPermutation(s1: str, s2: str) -> bool:
     if len(s1) != len(s2):
         return False
-    if sorted(s1) == sorted(s2):
-        return True
-    else:
+    
+    if sum(map(ord,s1)) != sum(map(ord,s2)):
         return False
+    
+    character_count = {}
+    for i in s1:
+        if i in character_count:
+            character_count[i] += 1
+        else:
+            character_count[i] = 1
+            
+    for j in s2:
+        if j in character_count:
+            if character_count[j] == 1:
+                del character_count[j] 
+            else:
+                character_count[j] -= 1
+                
+    if character_count == {}:
+        return True
+    
+    return False
 
 # Testing
 print(isStringPermutation("asdf", "fsda") == True)
 print(isStringPermutation("asdf", "fsa") == False)
 print(isStringPermutation("asdf", "fsax") == False)
-
 
 def pairsThatEqualSum(inputArray: list, targetSum: int) -> list:
     pairs = []
